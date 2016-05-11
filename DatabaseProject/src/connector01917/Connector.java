4f594b175f61966e2a83f147780f140fd14d6ca5
Sacha Behrend <sacha.behrend@gmail.com>
2016-05-11 11:21:41 +0200

    made some small database changes in the Constant and Connector class.

--- DatabaseProject/src/connector01917/Connector.java
+++ DatabaseProject/src/connector01917/Connector.java
@@ -33,7 +33,7 @@ public class Connector
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
 		
 		// get Connection-object via DriverManager
-		return (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/virksomhed", "root", "");
+		return (Connection) DriverManager.getConnection(url, username, password);
 	}
 	
 	private static Connection conn;
