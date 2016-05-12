1ca4323ba485b02d2ae0c8a183439380fefc37bb
Annika <e_hlers@live.dk>
2016-05-12 15:43:53 +0200

    Database class implementation.
    Andreas is a super villain.

---
+++ DatabaseProject/src/daoimpl01917/MySQLProduktBatchKompDAO.java
@@ -0,0 +1,71 @@
+package daoimpl01917;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import connector01917.Connector;
+import daointerfaces01917.DALException;
+import daointerfaces01917.ProduktBatchKompDAO;
+import dto01917.ProduktBatchDTO;
+import dto01917.ProduktBatchKompDTO;
+
+public class MySQLProduktBatchKompDAO implements ProduktBatchKompDAO {
+
+	@Override
+	public ProduktBatchKompDTO getProduktBatchKomp(int pbId, int rbId) throws DALException {
+		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatchkomponent WHERE pb_id = " + pbId + " AND rb_id = " + rbId);
+	    try {
+	    	if (!rs.first()) throw new DALException("Produktbatchkomponenten pb_id = " + pbId + ", rb_id = " + rbId + " findes ikke");
+	    	return new ProduktBatchKompDTO(pbId, rbId, rs.getDouble("tara"), rs.getDouble("netto"), rs.getInt("opr_id"));
+	    }
+	    catch (SQLException e) {throw new DALException(e); }
+	}
+
+	@Override
+	public List<ProduktBatchKompDTO> getProduktBatchKompList(int pbId) throws DALException {
+		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatchkomponent WHERE pb_id = " + pbId);
+	    try {
+	    	List <ProduktBatchKompDTO> list = new ArrayList<ProduktBatchKompDTO>();
+	    	while(rs.next()){
+	    		list.add(new ProduktBatchKompDTO(pbId, rs.getInt("rb_id"), rs.getDouble("tara"), 
+	    				rs.getDouble("netto"), rs.getInt("opr_id")));
+	    	}
+	    	return list;
+	    }
+	    catch (SQLException e) {throw new DALException(e); }
+	}
+
+	@Override
+	public List<ProduktBatchKompDTO> getProduktBatchKompList() throws DALException {
+		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatchkomponent");
+	    try {
+	    	List <ProduktBatchKompDTO> list = new ArrayList<ProduktBatchKompDTO>();
+	    	while(rs.next()){
+	    		list.add(new ProduktBatchKompDTO(rs.getInt("pb_id"), rs.getInt("rb_id"), rs.getDouble("tara"), 
+	    				rs.getDouble("netto"), rs.getInt("opr_id")));
+	    	}
+	    	return list;
+	    }
+	    catch (SQLException e) {throw new DALException(e); }
+	}
+
+	@Override
+	public void createProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent) throws DALException {
+		Connector.doUpdate(
+				"INSERT INTO produktbatchkomponent(pb_id, rb_id, tara, netto, opr_id) VALUES " +
+				"(" + produktbatchkomponent.getPbId() + ", " + produktbatchkomponent.getRbId() + ", " +
+						produktbatchkomponent.getTara() + ", " + produktbatchkomponent.getNetto() + ", " +
+						produktbatchkomponent.getOprId() + ")");
+	}
+
+	@Override
+	public void updateProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent) throws DALException {
+		Connector.doUpdate(
+				"UPDATE produktbatchkomponent SET tara = " + produktbatchkomponent.getTara() + ", netto = " +
+						produktbatchkomponent.getNetto() + ", opr_id = " + produktbatchkomponent.getOprId() +
+				"WHERE pb_id = " + produktbatchkomponent.getPbId() + " AND rb_id = " + produktbatchkomponent.getRbId());
+	}
+
+}
---
+++ DatabaseProject/src/daoimpl01917/MySQLReceptDAO.java
@@ -0,0 +1,53 @@
+package daoimpl01917;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import connector01917.Connector;
+import daointerfaces01917.DALException;
+import daointerfaces01917.ReceptDAO;
+import dto01917.ProduktBatchDTO;
+import dto01917.ReceptDTO;
+
+public class MySQLReceptDAO implements ReceptDAO {
+
+	@Override
+	public ReceptDTO getRecept(int receptId) throws DALException {
+		ResultSet rs = Connector.doQuery("SELECT * FROM recept WHERE recept_id = " + receptId);
+	    try {
+	    	if (!rs.first()) throw new DALException("Recepten " + receptId + " findes ikke");
+	    	return new ReceptDTO(receptId, rs.getString("recept_navn"));
+	    }
+	    catch (SQLException e) {throw new DALException(e); }
+	}
+
+	@Override
+	public List<ReceptDTO> getReceptList() throws DALException {
+		ResultSet rs = Connector.doQuery("SELECT * FROM recept");
+	    try {
+	    	List <ReceptDTO> list = new ArrayList<ReceptDTO>();
+	    	while(rs.next()){
+	    		list.add(new ReceptDTO(rs.getInt("recept_id"), rs.getString("recept_navn")));
+	    	}
+	    	return list;
+	    }
+	    catch (SQLException e) {throw new DALException(e); }
+	}
+
+	@Override
+	public void createRecept(ReceptDTO recept) throws DALException {
+		Connector.doUpdate(
+				"INSERT INTO recept(recept_id, recept_navn) VALUES " +
+				"(" + recept.getReceptId() + ", " + recept.getReceptNavn() + ")");
+	}
+
+	@Override
+	public void updateRecept(ReceptDTO recept) throws DALException {
+		Connector.doUpdate(
+				"UPDATE recept SET recept_navn = " + recept.getReceptNavn() + " WHERE recept_id = " + recept.getReceptId());
+		
+	}
+
+}
